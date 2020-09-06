f4f5fea925a72b4b42c322fca6988c09bdf32931
Davidson <davidsonts@outlook.com>
2020-09-05 23:06:19 -0300

    add kind on  Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,5 +28,20 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    if user
+      if user.kind == 'salesman'
+        can :access, :rails_admin
+        can :dashboard
+        can :manage, Client, user_id: user.id
+        can :manage, Sale, user_id: user.id
+        can :read, Product, status: :active
+        can :read, Discount, status: :active
+        can :read, Comission, user_id: user.id
+        can :manage, ProductQuantity, user_id: user.id
+        can :manage, Address, user_id: user.id
+      elsif user.kind == 'manager'
+        can :manage, :all
+      end
+    end
   end
 end
