18bf6a329d65adf0f1623210965b035507abc3f0
Davidson <davidsonts@outlook.com>
2020-09-06 21:51:51 -0300

    rubocop a

--- lib/rails_admin/rails_admin_pdf.rb
+++ lib/rails_admin/rails_admin_pdf.rb
@@ -1,9 +1,9 @@
 module RailsAdminPdf
 end
 
-require 'rails_admin/config/actions'
-require 'prawn'
-require 'gruff'
+require('rails_admin/config/actions')
+require('prawn')
+require('gruff')
 
 module RailsAdmin
   module Config
@@ -22,7 +22,8 @@ module RailsAdmin
         register_instance_option :controller do 
           Proc.new do 
             # Configurando PDF 
-            PDF_OPTIONS = { :page_size => "A4",
+            PDF_OPTIONS = { 
+              :page_size => "A4",
               :page_layout => :portrait,
               :margin      => [40, 75]
             }
@@ -31,92 +32,109 @@ module RailsAdmin
             ramdom_file_name = (0...8).map { (65 + rand(26)).chr }.join
 
             Prawn::Document.new(PDF_OPTIONS) do |pdf|
-              pdf.fill_color "666666"
-              pdf.text "Relatório do Representante", :size => 32, :style => :bold, :align => :center
-              pdf.move_down 80
+              pdf.fill_color("666666")
+              pdf.text("Relatório do Representante", :size => 32, :style => :bold, :align => :center)
+              pdf.move_down(80)
 
-              pdf.text "Dados Pessoais", :size => 14, :align => :justify, :inline_format => true, :style => :bold
-              pdf.move_down 14
+              pdf.text("Dados Pessoais", :size => 14, :align => :justify, :inline_format => true, :style => :bold)
+              pdf.move_down(14)
 
               if @object.name
-                pdf.text "Nome: #{@object.name}", :size => 12, :align => :justify, :inline_format => true
-                pdf.move_down 8
+                pdf.text("Nome: #{@object.name}", :size => 12, :align => :justify, :inline_format => true)
+                pdf.move_down(8)
               end
 
               if @object.document
-                pdf.text "Documento: #{@object.document}", :size => 12, :align => :justify, :inline_format => true
-                pdf.move_down 8
+                pdf.text("Documento: #{@object.document}", :size => 12, :align => :justify, :inline_format => true)
+                pdf.move_down(8)
               end
 
               if @object.kind
-                pdf.text "Tipo: #{@object.kind}", :size => 12, :align => :justify, :inline_format => true
-                pdf.move_down 8
+                pdf.text("Tipo: #{@object.kind}", :size => 12, :align => :justify, :inline_format => true)
+                pdf.move_down(8)
               end
 
               if @object.status
-                pdf.text "Status: #{@object.status}", :size => 12, :align => :justify, :inline_format => true
-                pdf.move_down 8
+                pdf.text("Status: #{@object.status}", :size => 12, :align => :justify, :inline_format => true)
+                pdf.move_down(8)
               end
 
               if @object.email
-                pdf.text "Email: #{@object.email}", :size => 12, :align => :justify, :inline_format => true
-                pdf.move_down 8
+                pdf.text("Email: #{@object.email}", :size => 12, :align => :justify, :inline_format => true)
+                pdf.move_down(8)
               end
 
-              pdf.move_down 20
+              pdf.move_down(20)
 
-              if @object.comissions.where(status: :pending).count > 0
+              if @object.comissions.where(:status => :pending).count > 0
 
-                pdf.text "Comissões Pendentes", :size => 14, :align => :justify, :inline_format => true, :style => :bold
-                pdf.move_down 14
+                pdf.text("Comissões Pendentes", :size => 14, :align => :justify, :inline_format => true, :style => :bold)
+                pdf.move_down(14)
 
                 total = 0
 
-                @object.comissions.where(status: :pending).each do |c|
-                  pdf.text "Id #{c.id}, valor R$#{c.value}, gerada em #{c.created_at.strftime("%d/%m/%y as %H:%M")}",
-                    :size => 12, :align => :justify, :inline_format => true
-                  pdf.move_down 8
+                @object.comissions.where(:status => :pending).each do |c|
+                  pdf.text(
+                    "Id #{c.id}, valor R$#{c.value}, gerada em #{c.created_at.strftime("%d/%m/%y as %H:%M")}",
+                    :size => 12, 
+:align => :justify, 
+:inline_format => true
+                  )
+                  pdf.move_down(8)
 
                   total += c.value
                 end
 
-                pdf.move_down 10
-                pdf.text "Total: R$#{total}", :size => 12, :align => :justify, :inline_format => true
-                pdf.move_down 20
+                pdf.move_down(10)
+                pdf.text("Total: R$#{total}", :size => 12, :align => :justify, :inline_format => true)
+                pdf.move_down(20)
               end
 
 
               if @object.clients.count > 0
 
-                pdf.text "Clientes Ativos", :size => 14, :align => :justify, :inline_format => true, :style => :bold
-                pdf.move_down 14
+                pdf.text("Clientes Ativos", :size => 14, :align => :justify, :inline_format => true, :style => :bold)
+                pdf.move_down(14)
 
                 total = 0
 
                 @object.clients.each do |c|
-                  pdf.text "#{c.name}", 
-                    :size => 12, :align => :justify, :inline_format => true, :style => :bold
-                  pdf.move_down 8
+                  pdf.text(
+                    "#{c.name}", 
+                    :size => 12, 
+:align => :justify, 
+:inline_format => true, 
+:style => :bold
+                  )
+                  pdf.move_down(8)
 
-                  pdf.text "Da empresa: #{c.company_name}",
-                    :size => 12, :align => :justify, :inline_format => true
-                  pdf.move_down 8
+                  pdf.text(
+                    "Da empresa: #{c.company_name}",
+                    :size => 12, 
+:align => :justify, 
+:inline_format => true
+                  )
+                  pdf.move_down(8)
 
-                  pdf.text "Cliente desde #{c.created_at.strftime("%d/%m/%y as %H:%M")}",
-                    :size => 12, :align => :justify, :inline_format => true
-                  pdf.move_down 8
+                  pdf.text(
+                    "Cliente desde #{c.created_at.strftime("%d/%m/%y as %H:%M")}",
+                    :size => 12, 
+:align => :justify, 
+:inline_format => true
+                  )
+                  pdf.move_down(8)
 
                   total += 1
                 end
 
-                pdf.move_down 10
-                pdf.text "Total: #{total}", :size => 12, :align => :justify, :inline_format => true
-                pdf.move_down 20
+                pdf.move_down(10)
+                pdf.text("Total: #{total}", :size => 12, :align => :justify, :inline_format => true)
+                pdf.move_down(20)
               end
 
-              if @object.sales.count  0
+              if @object.sales.count(0)
                 # Cria o objeto Gruff
-                g = Gruff::Pie.new 900
+                g = Gruff::Pie.new(900)
                 g.theme = Gruff::Themes::PASTEL
 
                 # Aqui ele formata nossos dados
@@ -134,18 +152,18 @@ module RailsAdmin
 
                 pdf.start_new_page
 
-                pdf.text "Gráfico de Vendas", :size => 20, :style => :bold, :align => :center
+                pdf.text("Gráfico de Vendas", :size => 20, :style => :bold, :align => :center)
 
                 # Incluir o gráfico numero 2
-                pdf.image "public/#{ramdom_file_name}_graph.jpg", :scale => 0.50
+                pdf.image("public/#{ramdom_file_name}_graph.jpg", :scale => 0.50)
               end
 
               # Muda de font para Helvetica
-              pdf.font "Helvetica"
+              pdf.font("Helvetica")
               # Inclui um texto com um link clicável (usando a tag link) no bottom da folha do lado esquerdo e coloca uma cor especifica nessa parte (usando a tag color)
-              pdf.text "Link<link href='https://github.com/Davidsonts/salesRails'>", :size => 10, :inline_format => true, :valign => :bottom, :align => :left
+              pdf.text("Link<link href='https://github.com/Davidsonts/salesRails'>", :size => 10, :inline_format => true, :valign => :bottom, :align => :left)
               # Inclui em baixo da folha do lado direito a data e o némero da página usando a tag page
-              pdf.number_pages "Gerado: #{(Time.now).strftime("%d/%m/%y as %H:%M")} - Página ", :start_count_at => 0, :page_filter => :all, :at => [pdf.bounds.right - 140, 7], :align => :right, :size => 8
+              pdf.number_pages("Gerado: #{(Time.now).strftime("%d/%m/%y as %H:%M")} - Página ", :start_count_at => 0, :page_filter => :all, :at => [pdf.bounds.right - 140, 7], :align => :right, :size => 8)
               # Gera no nosso PDF e coloca na pasta public com o nome agreement.pdf
               pdf.render_file("public/#{ramdom_file_name}.pdf")
             end
@@ -154,7 +172,7 @@ module RailsAdmin
               send_data f.read.force_encoding('BINARY'), :filename => 'pdf', :type => "application/pdf", :disposition => "attachment"
             end
             File.delete("public/#{ramdom_file_name}.pdf")
-            File.delete("public/#{ramdom_file_name}_graph.jpg") if @object.sales.count  0
+            File.delete("public/#{ramdom_file_name}_graph.jpg") if @object.sales.count(0)
           end
         end
 
